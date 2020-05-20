6ad23cb6497fa5b334b2c7c3f8858f2557d821d7
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-05-20 18:44:26 +0100

    add pre-commit hook for code formatting and style checks

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
@@ -6,7 +7,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
